5f46e420551c8085ad9fc379adf0c53991082e0f
Odunayo Ogungbure <odunayo@seamlesshr.com>
2025-05-02 00:39:06 +0100

    Add middleware for customer & supplier

---
+++ app/Http/Middleware/IsSupplierMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsSupplierMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->user()->isSupplier()) {
+            abort(403, 'Access denied');
+        }
+
+        return $next($request);
+    }
+}
